55eca8ceaf3ed8eefd7e8e820b69c835820f7d8d
Jan <janpipan@gmail.com>
2020-07-06 21:47:17 +0200

    resposnive nav

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+let nav = document.getElementsByClassName('vis-nav')[0];
+let overflow = document.getElementsByClassName('overflow')[0];
+let lastWidth;
+
+window.addEventListener('resize',()=>{
+  if(nav.scrollWidth > nav.clientWidth){
+    if(nav.lastChild.nodeName != '#text'){
+      let child = nav.lastChild;
+      overflow.appendChild(child);
+      child.classList.add('hidden');
+      lastWidth = nav.clientWidth;
+    }else{
+      nav.removeChild(nav.lastChild);
+    }
+  }
+  if(overflow.lastChild.nodeName == '#text'){
+    overflow.removeChild(overflow.lastChild);
+  }else if(nav.clientWidth-90-lastWidth>overflow.lastChild.clientWidth && overflow.lastChild.classList != 'more'){
+    let child = overflow.lastChild;
+    nav.appendChild(child);
+    lastWidth = nav.clientWidth;
+    console.log(lastWidth);
+  }
+  if(overflow.children.length == 1){
+    overflow.classList.add('hidden');
+  }else{
+    overflow.classList.remove('hidden');
+  }
+});
