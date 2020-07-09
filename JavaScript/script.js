80aca60cd15a379807c17522147e221948d99489
Jan <janpipan@gmail.com>
2020-07-09 20:10:58 +0200

    responsive nav with not fully working more button

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,9 @@ let nav = document.getElementsByClassName('vis-nav')[0];
 let overflow = document.getElementsByClassName('overflow')[0];
 let lastWidth;
 
+
 window.addEventListener('load', ()=>{
+  // displays right amount off items in nav bar overflow is hidden and transfered to more button
   if(nav.scrollWidth > nav.clientWidth){
     while(nav.scrollWidth > nav.clientWidth){
       if(nav.lastChild.nodeName != '#text'){
@@ -18,23 +20,30 @@ window.addEventListener('load', ()=>{
 });
 
 window.addEventListener('resize',()=>{
-  if(nav.scrollWidth > nav.clientWidth){
-    if(nav.lastChild.nodeName != '#text'){
-      let child = nav.lastChild;
-      overflow.appendChild(child);
-      child.classList.add('hidden');
-      lastWidth = nav.clientWidth;
-    }else{
-      nav.removeChild(nav.lastChild);
+// removes from visible nav and adds to more button
+  if(nav.scrollWidth+15 > nav.clientWidth){
+    while(nav.scrollWidth > nav.clientWidth){
+      if(nav.lastChild.nodeName != '#text'){
+        let child = nav.lastChild;
+        overflow.appendChild(child);
+        child.classList.add('hidden');
+        lastWidth = nav.clientWidth;
+      }else{
+        nav.removeChild(nav.lastChild);
+      }
     }
   }
+
   if(overflow.lastChild.nodeName == '#text'){
     overflow.removeChild(overflow.lastChild);
-  }else if(nav.clientWidth-90-lastWidth>overflow.lastChild.clientWidth && overflow.lastChild.classList != 'more'){
+  }else if(nav.clientWidth-(nav.children.length*20)-lastWidth>overflow.lastChild.clientWidth && overflow.lastChild.classList != 'more'){
     let child = overflow.lastChild;
     nav.appendChild(child);
-    lastWidth = nav.clientWidth;
+    lastWidth = nav.clientWidth+5;
+    child = overflow.lastChild;
+
   }
+
   if(overflow.children.length == 1){
     overflow.classList.add('hidden');
   }else{
